48a02c1c6cf2568cdbc0f13d7c12eace2229914d
benjamin <adolphe906@gmail.com>
2022-10-30 22:26:34 +0100

    Chapitre 3 : Structurer un document avec des directives
    - Conditionnez l'affichage des éléments
    - Affichez des listes
    - Ajoutez du style dynamique
    - Mettez de la classe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,40 @@ import { FaceSnap } from './Models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  faceSnaps!:FaceSnap[];
+
   mySnap!:FaceSnap;
   otherSnap!: FaceSnap;
   lastSnap!:FaceSnap;
 
   ngOnInit(): void {
-      this.mySnap = new FaceSnap(
+    this.faceSnaps = [
+      new FaceSnap(
         'Archibald',
         'Mon meilleur ami depuis tout petit',
         'https://cdn.pixabay.com/photo/2014/11/09/21/44/teddy-bear-524251_1280.jpg',
         new Date(),
-        0
-      );
-      this.otherSnap = new FaceSnap(
+        0,
+        'Paris'
+      ),
+      new FaceSnap(
         'Voyage dans les tribus',
         'Mamie souriante',
         'https://cdn.pixabay.com/photo/2021/05/23/15/12/old-woman-6276484_1280.jpg',
         new Date(),
         0
-      );
-      this.lastSnap = new FaceSnap(
+      ),
+      new FaceSnap(
         'Voyage dans les montagnes',
         'Photo du Sugarloaf Rock',
         'https://cdn.pixabay.com/photo/2022/10/01/22/38/sugarloaf-rock-7492389_1280.jpg',
         new Date(),
-        150000
-      );
+        150000,
+        'La Montagne'
+      )
+    ];
+    
   }
 
 }
